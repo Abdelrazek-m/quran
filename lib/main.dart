425a99d1c918cf23e4c2eb8f0bf178fc6ba6cf2a
Hossam eldeen Hassan <95591772+hossamlnemr@users.noreply.github.com>
2023-09-21 16:32:04 +0300

    adding assests

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,27 @@
+// ignore_for_file: unused_import, duplicate_import, depend_on_referenced_packages, use_key_in_widget_constructors, prefer_const_constructors
+
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html';
+
 import 'package:flutter/material.dart';
+import 'package:islamic_app/home_page.dart';
 
 void main() {
-  runApp( QuranApp());
+  runApp(MyApp());
 }
-class QuranApp extends StatelessWidget {
-  QuranApp({super.key});
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        fontFamily: 'Quicksand',
+        textTheme: ThemeData.light().textTheme.copyWith(
+            titleMedium: TextStyle(fontFamily: 'Quicksand', fontSize: 15.0)),
+      ),
+      home: home_page(),
+    );
   }
 }
