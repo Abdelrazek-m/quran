6de5ce7ab0934de6d042c1fe565df4f615523604
Abdelrazek <aalrazek02@gmail.com>
2023-09-22 22:36:22 +0300

    add azkars

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:quran/pages/azkar_page.dart';
 
 void main() {
-  runApp( QuranApp());
+  runApp( const QuranApp());
 }
 class QuranApp extends StatelessWidget {
   const QuranApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:AzkarPage(), 
+    );
   }
 }
